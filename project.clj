25c0707a8874a8a684509c758a389a31b4bd3ce3
zhujinxian <zhujinxian@xiaomi.com>
2015-02-12 16:58:54 +0800

    modify for clojure 1.3+

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject uk.org.alienscience/leiningen-war "0.0.15"
+(defproject zhu.road/leiningen-war "0.0.1"
   :description "Leiningen war plugin."
   :dependencies [[org.clojure/clojure "1.6.0"]
                      [org.clojure/core.incubator "0.1.3"]]
