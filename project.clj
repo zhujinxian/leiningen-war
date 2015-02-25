b441a81bc5e7c4e4c029c2c31cbc7e4da3dbc942
zhujinxian <zhujinxian@xiaomi.com>
2015-02-25 16:52:04 +0800

    modify project setting

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject zhu.road/leiningen-war "0.0.1"
+(defproject zhu/leiningen-war "0.0.1"
   :description "Leiningen war plugin."
   :dependencies [[org.clojure/clojure "1.6.0"]
                      [org.clojure/core.incubator "0.1.3"]]
